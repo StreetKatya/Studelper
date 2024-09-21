5c092e451538af19dff73cdd83cfb243e039fff2
StreetKatya <odintsova.2001@mail.ru>
2024-09-21 21:30:35 +0300

    add button

--- app/src/main/java/com/example/studelper/MainActivity.java
+++ app/src/main/java/com/example/studelper/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.studelper;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +17,7 @@ import com.example.studelper.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Button about, mark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +27,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        about = findViewById(R.id.about);
+//        about.setOnClickListener(new View.OnClickListener() {
+//                                     @Override
+//                                     public void onClick(View view) {
+//                                         // Действие при клике на кнопку
+//                                         navController.navigate(R.id.action_navigation_dashboard_to_navigation_about); //  Замените  на  ID  перехода  в  nav_graph.xml
+//                                     }
+//                                 });
+        mark = findViewById(R.id.mark);
+
+
     }
 
 }
